ae7ad2db1975c02b42c34f97f23cf9bcc3db7ed8
rendichs <rendi.febriyansyah@gmail.com>
2026-08-30 05:59:32 +0700

    Add Custom User Manager

--- api/models.py
+++ api/models.py
@@ -1,7 +1,25 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
-# Create your models here.
+class CustomUserManager(BaseUserManager):
+    def create_user(self, username, email=None, password=None, **extra_fields):
+        if not username:
+            raise ValueError("Username wajib diisi")
+            
+        user = self.model(username=username, email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, username, email=None, password=None, **extra_fields):
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+        
+        # Baris ini yang memastikan superuser otomatis menjadi admin
+        extra_fields.setdefault('role', 'admin')
+
+        return self.create_user(username, email, password, **extra_fields)
+
 class CustomUser(AbstractUser):
     ROLE_CHOICES = (
         ('admin', 'Admin'),
